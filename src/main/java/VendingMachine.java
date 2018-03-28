b0936dd1d86f2c4c34e0ec907fa0779b92f8a392
BrookeHau <brookerhauser@gmail.com>
2018-03-27 22:33:54 -0400

    return balance for adding coins to vm

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -4,14 +4,16 @@ import java.util.List;
 public class VendingMachine {
 
 	List<Coin> coins = Arrays.asList();
-	private double balance = 0 ;
+	private double balance;
 
 	public String display() {
 		return "INSERT COIN";
 	}
 
-	public void addCoin(Coin type) {
-		balance += type.value();
+	public void addCoin(Coin... coins) {
+		for (Coin coin : coins) {
+			balance += coin.value();
+		}
 	}
 
 	public double balance() {
--- src/test/java/VendingTest.java
+++ src/test/java/VendingTest.java
@@ -20,16 +20,36 @@ public class VendingTest {
 	public void displayTwentyFiveForQuarter() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.006, 1.5);
+		vm.addCoin(coin);
 		double bal = vm.balance();
 		assertThat(bal, is(0.25));
 	}
 	
-	/*@Test 
+	@Test 
 	public void displayTenForDime() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.004, 0.5);
 		vm.addCoin(coin);
 		double result = vm.balance();
 		assertThat(result, is(0.10));
-	}*/
+	}
+	
+	@Test 
+	public void displayTenForNickel() {
+		VendingMachine vm = new VendingMachine();
+		Coin coin = new Coin(.005, 1);
+		vm.addCoin(coin);
+		double result = vm.balance();
+		assertThat(result, is(0.05));
+	}
+	
+	@Test
+	public void valueForMultipleCoins() {
+		VendingMachine vm = new VendingMachine();
+		Coin coin = new Coin(.005, 1);
+		Coin coin2 = new Coin(.006, 1.5);
+		vm.addCoin(coin, coin2);
+		double result = vm.balance();
+		assertThat(result, is(0.30));	
+	}
 }
