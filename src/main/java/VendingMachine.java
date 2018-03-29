7c6add697734935bce65598994af1e9cdb903cff
BrookeHau <brookerhauser@gmail.com>
2018-03-28 20:39:11 -0400

    testing for using coins to buy products

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -6,13 +6,39 @@ import java.util.List;
 public class VendingMachine {
 
 	Collection<Coin> coins = new ArrayList<>();
+	Collection<Inventory> inventories = new ArrayList<>();
 	Collection<Product> products = new ArrayList<>();
-	
-	
+
+	public VendingMachine() {
+		Product chips = new Product("Chips", 0.50);
+		Inventory chipInv = new Inventory(chips, 5);
+		inventories.add(chipInv);
+		Product cola = new Product("Cola", 1.00);
+		Inventory colaInv = new Inventory(cola, 6);
+		inventories.add(colaInv);
+		Product candy = new Product("Candy", 0.65);
+		Inventory candyInv = new Inventory(candy, 9);
+		inventories.add(candyInv);
+	}
+
 	private double balance;
 
 	public String display() {
-		return "INSERT COIN";
+		if (balance == 0) {
+			return "INSERT COIN";
+		}
+		for (Product product : products) {
+			if (balance < product.price()) {
+				return "Price is " + product.price();
+			}
+			if (balance == product.price()) {
+				return "Thank you, here's your " + product.name();
+			} if(balance > product.price()) {
+				return "Your change is " + (balance - product.price());
+			}
+		}
+
+		return "You have " + balance;
 	}
 
 	public void addCoin(Coin... coins) {
@@ -24,25 +50,24 @@ public class VendingMachine {
 	public double balance() {
 		return balance;
 	}
-	
 
-	public void addProduct(Product product) {
+	public void selectProduct(Product product) {
 		products.add(product);
 	}
 
 	public String getProductName() {
-		for(Product product: products) {
+		for (Product product : products) {
 			return product.name();
 		}
 		return null;
-		
+
 	}
 
 	public double getProductPrice() {
-		for(Product product: products) {
+		for (Product product : products) {
 			return product.price();
 		}
-		return 0 ;
+		return 0;
 	}
 
 }
--- src/test/java/VendingTest.java
+++ src/test/java/VendingTest.java
@@ -5,53 +5,54 @@ import org.junit.Test;
 
 public class VendingTest {
 
-	
-	
-	
 	@Test
 	public void displayInsertCoin() {
 		VendingMachine vm = new VendingMachine();
 		String result = vm.display();
 		assertThat(result, is("INSERT COIN"));
 	}
-	
-	@Test 
+
+	@Test
 	public void displayTwentyFiveForQuarter() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.006, 1.5);
 		vm.addCoin(coin);
-		double bal = vm.balance();
-		assertThat(bal, is(0.25));
+		/*double bal = vm.balance();*/
+		String bal = vm.display();
+		assertThat(bal, is("You have 0.25"));
 	}
-	
-	@Test 
+
+	@Test
 	public void displayTenForDime() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.004, 0.5);
 		vm.addCoin(coin);
-		double result = vm.balance();
-		assertThat(result, is(0.10));
+	/*	double result = vm.balance();*/
+		String result = vm.display();
+		assertThat(result, is("You have 0.1"));
 	}
-	
-	@Test 
+
+	@Test
 	public void displayTenForNickel() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.005, 1);
 		vm.addCoin(coin);
-		double result = vm.balance();
-		assertThat(result, is(0.05));
+		/*double result = vm.balance();*/
+		String result = vm.display();
+		assertThat(result, is("You have 0.05"));
 	}
-	
+
 	@Test
 	public void valueForMultipleCoins() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.005, 1);
 		Coin coin2 = new Coin(.006, 1.5);
 		vm.addCoin(coin, coin2);
-		double result = vm.balance();
-		assertThat(result, is(0.30));	
+		/*double result = vm.balance();*/
+		String result = vm.display();
+		assertThat(result, is("You have 0.3"));
 	}
-	
+
 	@Test
 	public void addMoreCoins() {
 		VendingMachine vm = new VendingMachine();
@@ -60,10 +61,10 @@ public class VendingTest {
 		Coin coin3 = new Coin(.006, 1.5);
 		vm.addCoin(coin, coin2, coin3);
 		double result = vm.balance();
-		assertThat(result, is(0.55));	
+		assertThat(result, is(0.55));
 	}
-	
-	@Test 
+
+	@Test
 	public void shouldDisplayZero() {
 		VendingMachine vm = new VendingMachine();
 		Coin coin = new Coin(.003, 1.5);
@@ -71,23 +72,59 @@ public class VendingTest {
 		double bal = vm.balance();
 		assertThat(bal, is(0.0));
 	}
-	
+
 	@Test
 	public void addProductToVendingOrder() {
 		Product product = new Product("Chips", 1.50);
 		VendingMachine vm = new VendingMachine();
-		vm.addProduct(product);
+		vm.selectProduct(product);
 		String check = vm.getProductName();
 		assertThat(check, is("Chips"));
 	}
-	
+
 	@Test
 	public void getProductPrice() {
 		Product product = new Product("Chips", 1.50);
 		VendingMachine vm = new VendingMachine();
-		vm.addProduct(product);
+		vm.selectProduct(product);
 		double check = vm.getProductPrice();
 		assertThat(check, is(1.50));
 	}
 	
+	@Test
+	public void tryToBuyProductWithLessCoins() {
+		VendingMachine vm = new VendingMachine();
+		Coin coin = new Coin(.006, 1.5);
+		Coin coin2 = new Coin(.004, 0.5);
+		vm.addCoin(coin, coin, coin, coin, coin);
+		Product chips = new Product("Chips", 1.50);
+		vm.selectProduct(chips);
+		String result = vm.display();
+		assertThat(result, is("Price is 1.5"));	
+	}
+	
+	@Test
+	public void tryToBuyProductWithExactChange() {
+		VendingMachine vm = new VendingMachine();
+		Coin coin = new Coin(.006, 1.5);
+		Coin coin2 = new Coin(.004, 0.5);
+		vm.addCoin(coin, coin, coin, coin, coin, coin);
+		Product chips = new Product("Chips", 1.50);
+		vm.selectProduct(chips);
+		String result = vm.display();
+		assertThat(result, is("Thank you, here's your Chips"));	
+	}
+	
+	@Test
+	public void buyProductWithTooManyCoins() {
+		VendingMachine vm = new VendingMachine();
+		Coin coin = new Coin(.006, 1.5);
+		Coin coin2 = new Coin(.004, 0.5);
+		vm.addCoin(coin, coin, coin, coin, coin, coin, coin);
+		Product chips = new Product("Chips", 1.50);
+		vm.selectProduct(chips);
+		String result = vm.display();
+		assertThat(result, is("Your change is 0.25"));	
+	}
+
 }
